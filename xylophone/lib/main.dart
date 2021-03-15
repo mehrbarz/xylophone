07515a089b919e3b0b17e7ca6882a5a73e43f8e0
mehrbarz <mehrbarzegar@gmail.com>
2021-03-15 12:45:07 +0100

    click to play button

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Text(nouns.first),
+          child: FlatButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note5.wav');
+            },
+            child: Text('Click to play'),
+          ),
         ),
       ),
     );
